ceeac7250cf66853e550e0dcb3d038ed77f16367
Michael Gerges <michael.george.mik@gmail.com>
2025-09-11 23:38:26 -0400

    created the middlewares

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { logger } from "./middleware/logger.js";
+import { notFound, errorHandler } from "./middleware/errorHandler.js";
 import brandsRoute from "./routes/brandsRoute.js";
 import appleRoute from "./routes/appleRoute.js";
 import issuesRoute from "./routes/issuesRoute.js";
@@ -10,10 +12,15 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(logger);
+
 app.use("/api/brands/apple/issues", issuesRoute);
 app.use("/api/brands/apple", appleRoute);
 app.use("/api/brands", brandsRoute);
 app.use("/api/tickets", ticketsRoute);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
