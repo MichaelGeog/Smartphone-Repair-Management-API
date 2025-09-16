de3da42ef4e4f972c5052238658e67025eb79531
Michael Gerges <michael.george.mik@gmail.com>
2025-09-16 01:13:42 -0400

    changed the absolute path to be dynamic

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { logger } from "./middleware/logger.js";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
+
 import brandsRoute from "./routes/brandsRoute.js";
 import appleRoute from "./routes/appleRoute.js";
 import issuesRoute from "./routes/issuesRoute.js";
@@ -8,26 +12,31 @@ import ticketsRoute from "./routes/ticketsRoute.js";
 import ticketsPageRoute from "./routes/ticketsPageRoute.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger);
 
 app.set("view engine", "ejs");
-app.set("views", "views");
-app.use(express.static("public"));
-
-app.use(logger);
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/brands/apple/issues", issuesRoute);
 app.use("/api/brands/apple", appleRoute);
 app.use("/api/brands", brandsRoute);
 app.use("/api/tickets", ticketsRoute);
+
 app.use("/", ticketsPageRoute);
 
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log("views =>", path.join(__dirname, "views"));
+  console.log("public =>", path.join(__dirname, "public"));
 });
